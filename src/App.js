7d55c79c86ce6e2a6180ae75758fb68ba2ed4f57
ConnorTsui <cjtsui@andrew.cmu.edu>
2022-10-01 11:13:19 -0400

    move compoenents

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            {/* <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,10 @@ function App() {
                 >
                     Learn React
                 </a>
-            </header>
+            </header> */}
+            <video>
+                <source src="https://firebasestorage.googleapis.com/v0/b/hackmit22.appspot.com/o/videos%2Fshorts%2Fmixkit-small-flowering-plants-in-a-nursery-43709.mp4?alt=media&token=8ccfe3b8-6b33-40da-ae06-b02275f9749f" type="video/mp4" />
+            </video>
         </div>
     );
 }
