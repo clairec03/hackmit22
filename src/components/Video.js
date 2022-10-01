c039ff1c27fb1cbd1edb6e11dd0ee16ff8816e86
clairec03 <clairecchen03@gmail.com>
2022-10-01 12:30:12 -0400

    added fluid container

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,8 +1,10 @@
 export default function Video({ src }) {
     return (
+        <div class="container-fluid">
         <video controls>
             <source src={src} type="video/mp4" />
             Sorry, your browser doesn't support videos.
         </video>
-    );
+        </div>
+   );
 }
